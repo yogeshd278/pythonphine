cdb9bd8b54e1a295ff21471b05b88c26f381c06c
Yogesh Dubey <d16@cybuzzsc.in>
2020-05-17 23:53:27 +0530

    matplotlib scatter example

--- matplot.py
+++ matplot.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
 
-
 year = [1950, 1970, 1990, 2010]
 pop = [2.519, 3.692, 5.263, 6.972]
 
-plt.plot(year, pop)
+#plt.plot(year, pop)
+#plt.show()
+
+plt.scatter(year, pop)
 plt.show()
