1d702fdf99c9545c1c2edc04ae16085e88413582
Yogesh Dubey <d16@cybuzzsc.in>
2020-05-19 01:22:40 +0530

    loc & iloc example in pandas

--- panda_example2.py
+++ panda_example2.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
-data = pd.read_csv('./demo_files/sales_records.csv')
+#data = pd.read_csv('./demo_files/sales_records.csv')
 
-sales = pd.DataFrame(data)
+#sales = pd.DataFrame(data)
+#print(sales)
+
+
+data = pd.read_csv('./demo_files/sales_records.csv', index_col=0)
+#sales = pd.DataFrame(data[['Country']])
+#sales = pd.DataFrame(data[['Country','Total Profit']])
+
+## use loc & iloc
+#sales = data.loc[['Asia']]
+sales = data.iloc[[5]]
+
 print(sales)
