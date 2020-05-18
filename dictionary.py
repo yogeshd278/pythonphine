6a65ce4929122579ffe502398a9f6ccb7af7e23a
Yogesh Dubey <d16@cybuzzsc.in>
2020-05-18 23:58:03 +0530

    dictionaries example

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,20 @@
 # dictionaries
-world = {'india': 130, 'china': 200, 'USA': 43}
-print(world['china'])
+#world = {'india': 130, 'china': 200, 'USA': 43}
+#print(world['china'])
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+
+#print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = {'capital': 'rome', 'population': 59.83}
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+
+print(europe)
